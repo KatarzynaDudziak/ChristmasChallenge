82eff727911c99143cbd5cca748dd43bf6b301f9
Kasia <katarzynadudziak94@gmail.com>
2022-12-19 11:36:14 +0100

    Code refactor

--- Day19_Wrapping_paper.py
+++ Day19_Wrapping_paper.py
@@ -4,20 +4,30 @@ class Box:
         self.length = length
         self.width = width
 
-    def calculate_needed_wrapping_paper(self):
-        return round((2 * ((self.height * self.length) + (self.height * self.width)
-                 + (self.length * self.width)))/100)
+    def __repr__(self):
+        return(f'{self.height}, {self.length}, {self.width}')
+
+    def calculate_needed_wrapping_paper_for_one_box(self):
+        return (2 * ((self.height * self.length) + (self.height * self.width)
+                 + (self.length * self.width))/10000)
 
 
 def main():
+    small = Box(10, 13, 15),
+    medium = Box(20, 15, 10),
+    big = Box(30, 25, 20)
+
     dict_of_boxes = {
-        'small' : Box(10, 13, 15),
-        'medium' : Box(20, 15, 10),
-        'big' : Box(30, 25, 20)
+        5 : Box(10, 13, 15),
+        10 : Box(20, 15, 10),
+        3 : Box(30, 25, 20)
     }
 
-    for name, box in dict_of_boxes.items():
-        print(f'We need {box.calculate_needed_wrapping_paper()} m of paper to pack {name} box.')
+    sum_of_needed_wrapping_paper = 0
+
+    for number_of_boxes, box in dict_of_boxes.items():
+        sum_of_needed_wrapping_paper += (box.calculate_needed_wrapping_paper_for_one_box()) * number_of_boxes
+    print(f'We need {round(sum_of_needed_wrapping_paper)}m2 of paper to pack all the boxes.')
 
 if __name__=='__main__':
     main()
